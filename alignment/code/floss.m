3118b4d27508e50cea50d93b660fa872af92a635
yuyanislearning <56214384+yuyanislearning@users.noreply.github.com>
2019-11-18 20:34:57 -0800

    gradient descend closet point

--- alignment/code/floss.m
+++ alignment/code/floss.m
@@ -1,13 +1,13 @@
-function loss = floss(point_2d,project_point_2d, lambda, theta_x, theta_y, theta_z)
+function loss = floss(point_2d,project_point_2d, lambda, theta_x, theta_y, theta_z, index_2d)
     % calculate the loss
-    % brute distance, calculate distance from all pairwise point
+    % distance for the closet point
     loss = 0;
-    for i = 1:size(point_2d,2)
-        for j = 1:size(project_point_2d,2)
-            loss= loss + sum((point_2d(:,i)-project_point_2d(:,j)).^2);
-        end
+    for i = 1:size(project_point_2d,2)
+        x= sum((point_2d(1,index_2d)-project_point_2d(1,:)).^2);
+        y= sum((point_2d(2,index_2d)-project_point_2d(2,:)).^2);
+        loss = x+y;
     end
-    loss = loss/size(point_2d,2)/size(project_point_2d,2);
+    loss = loss/size(project_point_2d,2);
     loss = loss + 1/2*lambda*(theta_x^2+theta_y^2+theta_z^2);
 end
 
