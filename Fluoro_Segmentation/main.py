40b2553b7f798bb97468e4cad1600f333dde7d91
Anil Yadav <ayadav01@student.coppin.edu>
2019-12-09 17:13:08 -0800

    changes made to run for all patients

--- Fluoro_Segmentation/main.py
+++ Fluoro_Segmentation/main.py
@@ -1,13 +1,15 @@
 #import the file
-import pin_tips_extract as ptx
+import localize_pins as ptx
 
-#path to file
-path_to_fluro = "subject/fluoro_subject_1.tif"
+#provide path to the root folder that has sub folders of all subjects
+path_to_root = "C:/user/all_subject_data"
+#path to ouput folder
+output = "C:/user/output/"
 
 #create an object
-fluro_object = ptx.PinTips(path_to_fluro)
+fluro_object = ptx.PinTips_All(path_to_root, output)
 
 #extract the coordinates (variable will contain a tuple of arrays corresponding to x and y coordinates)
-pin_coord = fluro_object.extract_pin()
+fluro_object.extract_pin()
 
-print(pin_coord)
+
